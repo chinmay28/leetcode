11fad34f7545ecde3dbc4dc2828731fe7001b3fd
Chinmay Manjunath <cmanjunath@purestorage.com>
2019-06-12 18:37:42 -0700

    2. Add Two Numbers

---
+++ cpp/problems/add_two_numbers.cpp
@@ -0,0 +1,45 @@
+/**
+ * 2. Add Two Numbers
+ * 
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * 
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode result(0); // first element stays on the stack
+        ListNode *curr = &result;
+        int carry = 0;
+        while (l1 || l2 || carry)
+        {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            curr->next = new ListNode(sum % 10);
+            carry = sum / 10;
+            curr = curr->next;
+            l1 = l1 ? l1->next : NULL;
+            l2 = l2 ? l2->next : NULL;
+        }
+        return result.next;
+    }
+};
