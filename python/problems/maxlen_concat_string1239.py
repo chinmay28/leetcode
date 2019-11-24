c4a185dfcac315c040cc4e11d100db33cc034915
Chinmay Manjunath <cmanjunath@purestorage.com>
2019-11-23 18:16:25 -0800

    1239 - added a DP solution

--- python/problems/maxlen_concat_string1239.py
+++ python/problems/maxlen_concat_string1239.py
@@ -38,20 +38,35 @@ class Solution(object):
         :type arr: List[str]
         :rtype: int
         """
-        return self.backtrack(arr, 0, '', 0)
+        #return self.backtrack(arr, 0, '', 0)
+        return self.dp_using_sets(arr)
 
     def backtrack(self, string_list, i, current_string, result):
-        
+
         if i == len(string_list):
             # we have reached the end of the string list
             result = max(result, len(current_string))
             return result
-        
+
         if len(set(string_list[i])) == len(string_list[i]):
             # ith string has unique characters
             if all(char not in current_string for char in string_list[i]):
-                result = self.backtrack(string_list, i + 1, 
+                result = self.backtrack(string_list, i + 1,
                                         current_string + string_list[i], result)
 
         result = self.backtrack(string_list, i+1, current_string, result)
         return result
+
+    def dp_using_sets(self, array):
+
+        dp = [set()]
+        for string in array:
+            if len(set(string)) != len(string):
+                continue
+            for dp_string_set in dp:
+                if not dp_string_set & set(string):
+                    dp.append(dp_string_set | set(string))
+
+        return max(len(string_set) for string_set in dp)
+
+
